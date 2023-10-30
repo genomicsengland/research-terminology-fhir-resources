c14929d49d9fd8221ed7e355d22f9530f9ec5c95
rickreesrr <rick.rees@genomicsengland.co.uk>
2023-10-30 16:18:05 +0000

    build acronym override list and fixed code system

--- src/main/python/sact/drug_codesystem_from_list.py
+++ src/main/python/sact/drug_codesystem_from_list.py
@@ -5,6 +5,10 @@
 import json
 from titlecase import titlecase
 
+def abbreviations(word, all_caps):
+    acronyms_to_leave_as_is = ['ADCT-301', 'ATG', 'AZD1775', 'CAR-T', 'PDR001']
+    return word if word.upper() in acronyms_to_leave_as_is else titlecase(word)
+
 input_file = './drug_list_from_db.txt'
 output_file = './sact-drug-group.json'
 
@@ -14,7 +18,7 @@ with open(input_file) as input:
         drugs.append(line.strip())
 
 concept_structure = {
-    "concept": [{"code": drug, "display": titlecase(drug)} for drug in drugs]
+    "concept": [{"code": drug, "display": titlecase(drug, callback=abbreviations)} for drug in drugs]
 }
 
 with open(output_file, 'w') as output:
